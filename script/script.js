f85c84c6378b9f78cda7eaa94500fe98a6ecf0ca
francescapetra <francescalaurapetra@gmail.com>
2021-04-09 14:35:45 +0200

    struttura test su film

--- script/script.js
+++ script/script.js
@@ -2,10 +2,26 @@
   {
     el:"#root",
     data:{
-      message: "",
-      miaClasse : "red",
+      films: [
+      ],
+      searchFilm:"",
     },
-    methods:{
+    methods: {
+      apiSearch:function(){
+        console.log("pippo");
+        axios.get("https://api.themoviedb.org/3/search/movie",{
+          params:{
+            api_key:"143011c614d2d290369cd7730a404df2",
+            query:"ritorno al futuro",
+            language: "it-IT"
+          }
+        })
+        .then((request)=>{
+          const result = request.data.results;
+          this.films = result;
+          console.log(result);
+        });
+      }
     },
   }
 );
