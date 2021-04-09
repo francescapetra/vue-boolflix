4412db0cbe2240a3add5e7f6c6630a5d59a10593
francescapetra <francescalaurapetra@gmail.com>
2021-04-09 19:18:22 +0200

    star rate

--- script/script.js
+++ script/script.js
@@ -2,8 +2,7 @@
   {
     el:"#root",
     data:{
-      films: [
-      ],
+      films: [],
       searchFilm:"",
     },
     methods: {
@@ -20,8 +19,17 @@
           const result = request.data.results;
           this.films = result;
           console.log(result);
+          this.filmRate();
         })
         this.searchFilm = "";
+      },
+      filmRate: function(){
+        this.films.forEach((item, i) => {
+          const vote = (item.vote_average/2);
+          let starVote = Math.ceil(vote);
+          item.vote_average = starVote;
+        });
+
       }
     },
   }
